33c96bccc1443c0429834fb0af168e40c167efb4
Adalberto 'Nick' Garza <adagar121987@gmail.com>
2018-04-01 17:37:00 -0400

    First draft of finance page

--- scripts/index.js
+++ scripts/index.js
@@ -1,60 +1,24 @@
 $(document).ready(function() {
-    console.log("ready!");
-    //Setting temperature units
-    var tempUnit = "C";
-
-    var lat;
-    var long;
-
-    function setLocation(position){
-        lat = position.coords.latitude;
-        long = position.coords.longitude;
-        displayWeather();
-    }
-
-    function displayWeather(){
-        $.getJSON("https://fcc-weather-api.glitch.me/api/current?lat=" + lat + "&lon=" + long, function(json){
-        //$("#weather").html(JSON.stringify(json));
-        
-        var html = "";
-
-        //json.forEach(function(val){
-        var keys = Object.keys(json);
-
-        if(tempUnit == "C"){
-          var currTemp = json["main"]["temp"];
-        } else{
-          var currTemp = ((Number(json["main"]["temp"]) * 9) / 5) + 32;
-        }
-
-
-        //Print importnat weather data
-        html += "<div class = 'weather-data'>";
-        html += "<p id='location'>" + json["name"] + ", " + json["sys"]["country"] + "</p>";
-        html += "<p id = 'temp'> Current Temperature: " + currTemp + "&deg;" + tempUnit;
-        html += "<p id = 'cloudCov'> Cloud Cover: " + json["weather"][0]["main"] + "</p>";
-        html += "<img src = '" + json["weather"][0]["icon"] + "'>"; //TODO: Integrate these icons https://darkskyapp.github.io/skycons/
-
-        html += "</div><br>";
-        $("#weather-data").html(html);  
-      });
-    } 
-
-    $("#temp-select input:radio").click(function(){
-      tempUnit = $('input[name=weather-units]:checked').val();
-      alert("Changing temp units to: " + tempUnit);
-      displayWeather();
-    });
-
-    function getLocation(){
-      if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(setLocation);
+  function changeColor(){
+    /*
+    var elems = document.getElementsByClassName("change");
+    console.log(elems);
+    for(var i in elems){
+     if(elems[i].value >= 0){
+        elems[i].addClass("increasing");
       }else{
-        $("#weather").html("Geolocation is not supported by this browser, but I hope its nice out.");
-      }
+        elems[i].addClass("decreasing");
+      } 
     }
-
-    
-
-    getLocation();
+    */
+    $(".change").each(function(index){
+      console.log($(this).html());
+      if($(this).html() >= 0){
+        $(this).addClass("increasing");
+      }else{
+        $(this).addClass("decreasing");
+      }    
+    });
+  };
+  changeColor();
 });
