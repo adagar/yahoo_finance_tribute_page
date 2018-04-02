11bc452d77c93ee0928e0fe5ff2ef202d5a206b0
Adalberto 'Nick' Garza <adagar121987@gmail.com>
2018-04-02 18:35:52 -0400

    Home page added

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,5 @@
 $(document).ready(function() {
   function changeColor(){
-    /*
-    var elems = document.getElementsByClassName("change");
-    console.log(elems);
-    for(var i in elems){
-     if(elems[i].value >= 0){
-        elems[i].addClass("increasing");
-      }else{
-        elems[i].addClass("decreasing");
-      } 
-    }
-    */
     $(".change").each(function(index){
       console.log($(this).html());
       if($(this).html() >= 0){
